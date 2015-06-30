8362b056f86cc711735d6fe0b987a7522eb9d479
stkevintan <stkevintan@foxmail.com>
2015-06-30 20:16:41 +0800

    add init

--- src/my.js
+++ src/my.js
@@ -1,3 +1,17 @@
 /**
  * Created by kevin on 15-6-30.
  */
+var gui = require('nw.gui');
+var win = gui.Window.get();
+$(document).foundation();
+function createDOM(name, opts, inner) {
+    var dom = document.createElement(name);
+    for (var key in opts) {
+        dom.setAttribute(key, opts[key]);
+    }
+    if (typeof inner !== 'undefined') {
+        dom.innerText = inner;
+    }
+    return dom;
+}
+
